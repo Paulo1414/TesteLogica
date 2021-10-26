7c7ff152ad3bc8420658ee4daadaa68894477adc
Paulo1414 <70149243+Paulo1414@users.noreply.github.com>
2021-10-26 01:22:47 -0300

    correcao

---
+++ projetosPyton/exercicio_4.py
@@ -0,0 +1,17 @@
+SP = 67.83643
+RJ =36.67866
+MG =29.22988
+ES =27.16548
+Outros =19.84953
+
+calcSP = (67.83643*100)/180.75998
+calcRJ = (36.67866*100)/180.75998
+calcMG = (29.22988*100)/180.75998
+calcES = (27.16548*100)/180.75998
+calcOutros = (19.84953*100)/180.75998
+print("porcentagem SP {} ".format(calcSP))
+print("porcentagem RJ {} ".format(calcRJ))
+print("porcentagem MG {} ".format(calcMG))
+print("porcentagem ES {} ".format(calcES))
+print("porcentagem Outros {} ".format(calcOutros))
+
